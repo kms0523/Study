47bacd841003401fe818ecc6be7248640f4a3025
Kim Minseok <rla523at@naver.com>
2022-12-16 00:23:20 +0900

    Jupyter book 관련 수정

--- _code/Python/Jupyterbook/TOC_Writer.py
+++ _code/Python/Jupyterbook/TOC_Writer.py
@@ -1,13 +1,6 @@
 from .mslib import mspath
 
-# section 이름과 동일한 .md 파일이 main section 파일이 된다.
-# TOC 파일에 적힌 순서대로 toc가 생성
-
 class TOC_Writer:
-	def __init__(self) -> None:
-		pass
-
-
 	def write(self, source_folder_path) -> None:
 		mspath.check_path_format(source_folder_path)
 		mspath.check_path_existence(source_folder_path)		
@@ -17,18 +10,22 @@ class TOC_Writer:
 
 		self.__write_header(file)
 
-		names = mspath.extract_names(source_folder_path)
+		names = mspath.extract_foldernames(source_folder_path)
 
+		# Documents
 		for name in names:			
 			if name.startswith('_'): #convetion
 				continue
 
-			path = source_folder_path + name
-			if mspath.is_folder(path):
-				chapter_folder_path = source_folder_path + name + '/'
-				self.__write_chapter(file, chapter_folder_path)
+			chapter_folder_path = source_folder_path + name + '/'
+			self.__write_chapter(file, chapter_folder_path)
 
 		file.close()
+
+
+
+	def __init__(self) -> None:
+		pass
 
 
 
@@ -47,56 +44,64 @@ class TOC_Writer:
 		mspath.check_path_existence(current_path)
 
 		indent = '  '
-		caption = mspath.current_folder_name(current_path) #convention
+		caption = mspath.current_folder_name(current_path)[3:] #convention
 		file.write(indent + '- caption: \'' + caption + '\'\n')
 		
+		name_extension_tuples = mspath.extract_name_extension_tuples(current_path)
+
+		#Processing documents
 		indent = indent + '  '
 		file.write(indent + 'chapters: ' + '\n')
-				
-		names = mspath.extract_names(current_path)
 
-		for name in names:
-			if name.startswith('_'): #convetion
-				continue
-
-			path = current_path + name			
-			if mspath.is_folder(path):
-				section_folder_path = path + '/'
-				self.__write_section(file, indent, section_folder_path)
-			else:
-				self.__write_file(file,indent,path)
+		self.__write_in_TOC_format(file, indent,current_path, name_extension_tuples)
 
 
 
 	def __write_section(self, file, indent, current_path):
-		names = mspath.extract_names(current_path)
+		mspath.check_path_format(current_path)
+		mspath.check_path_existence(current_path)
 
-		# Processing Main Document
-		main_file_name = mspath.current_folder_name(current_path) #convention
+		name_extension_tuples = mspath.extract_name_extension_tuples(current_path)
+		
+		# Processing main document
+		main_file_name = mspath.current_folder_name(current_path)[3:] #convention
 		main_file_path = current_path + main_file_name
 		self.__write_file(file, indent, main_file_path)
 		
-		names.remove(main_file_name)
+		name_extension_tuples.remove((main_file_name, '.md'))
 
 		# Processing other documents
 		indent = indent + '  '
 		file.write(indent + 'sections: \n')
 
-		for name in names:
-			if name.startswith('_'): #convetion
-				continue
+		self.__write_in_TOC_format(file, indent,current_path, name_extension_tuples)
 
-			path = current_path + name			
-			if mspath.is_folder(path):
-				section_folder_path = path + '/'
-				self.__write_section(file, indent, section_folder_path)
-			else:
-				self.__write_file(file,indent,path)
+
 
 
 
 	def __write_file(self, file, indent, path) -> None:
 		file.write(indent + '- file: ' + path + '\n')
+
+
+
+
+
+	def __write_in_TOC_format(self, file, indent, current_path, name_extension_tuples) -> None:
+		for [name, extension] in name_extension_tuples:
+			if name.startswith('_'): #convetion
+				continue
+
+			if extension == '':
+				section_folder_path = current_path + name + '/'
+				self.__write_section(file, indent, section_folder_path)
+			elif extension == '.md':
+				path = current_path + name				
+				self.__write_file(file,indent,path)	
+
+
+
+
 
 
 	def __relative_path(self, path) :
